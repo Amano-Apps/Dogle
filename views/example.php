6528a1041b695594acd5e194ba7fa8ef478bad53
ShiroAky <martinezfranklinjunior7@gmail.com>
2022-11-11 18:38:23 -0400

    V1.0.0

--- views/example.php
+++ views/example.php
@@ -6,9 +6,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dogle - Framework</title>
 </head>
+<style type="text/css">
+    @import url('https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css');
+    * { 
+        margin: 0px; 
+        padding: 0px; 
+        border: none; 
+        outline: none; 
+    }
+    body { 
+        width: 100%; 
+        height: 100vh; 
+        display: flex; 
+        flex-direction: column; 
+        align-items: center; 
+        justify-content: center; 
+        background-color: #293241; 
+        color: #f2f2f2; 
+    }
+    h1 {
+        display: flex;
+        align-items: center;
+        align-content: center;
+        justify-items: center;
+        gap: 8px;
+    }
+    .ion-md-heart {
+        color: red;
+    }
+</style>
 <body>
 
-    <h1>Wellcome to Dogle Framework</h1>
+    <h1>Wellcome to Dogle Framework <i class="icon ion-md-heart"></i></h1>
     
 </body>
 </html>
